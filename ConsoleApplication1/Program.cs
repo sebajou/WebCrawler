ac8796fb3d8b2f08489767ccf195b08d90f7a43e
Larsen128 <bruttimairesse@gmail.com>
2015-10-16 15:56:27 +0200

    new

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    using System.IO;
+    using System.Net;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url  = Console.ReadLine();
+
+            WebRequest webRequest = WebRequest.Create(url);
+
+            Stream objStream = webRequest.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+
+            string line = string.Empty;
+            int i = 0;
+
+            while (line != null)
+            {
+                i++;
+                line = objReader.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine("{0}:{1}", i, line);
+                }
+            }
+
+            Main(args);
+        }
+    }
+}
